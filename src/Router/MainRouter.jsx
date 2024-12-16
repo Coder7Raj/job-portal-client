bf34eedbf4e07327ae4404b8fcb9c561002001b6
Coder7Raj <alwaysraj733@gmail.com>
2024-12-16 21:33:07 +0600

    user authentication acivated by firebase

--- src/Router/MainRouter.jsx
+++ src/Router/MainRouter.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home";
+import Root from "../Pages/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>,
+    element: <Root></Root>,
+    errorElement: <h1>error</h1>,
+    children: [{ path: "/", element: <Home></Home> }],
   },
 ]);
 
